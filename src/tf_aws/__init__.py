5daa9eb62d9f68c178a92ec98a6a92abfb3427e5
tomarv2 <varuntomar2019@gmail.com>
2021-03-01 14:22:26 -0800

    .

--- src/tf_aws/__init__.py
+++ src/tf_aws/__init__.py
@@ -2,6 +2,5 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 from ._version import __version__
-
 from src.tf_aws.plugin import TerraformAWSWrapper
 plugin = TerraformAWSWrapper()
