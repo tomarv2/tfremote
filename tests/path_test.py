3c70018dca60c0226599a39d75578a3e8fbd25fc
tomarv2 <varuntomar2019@gmail.com>
2021-03-01 21:58:13 -0800

    testing

--- tests/path_test.py
+++ tests/path_test.py
@@ -2,6 +2,9 @@ from src.common.run_command import create_command
 
 
 def test_create_command():
-    output = create_command(["tf", "plan", "-cloud", "aws", "-var-file", "foo.vars", "-var", "foo=var"])
+    output = create_command(
+        ["tf", "plan", "-cloud", "aws", "-var-file", "foo.vars", "-var", "foo=var"]
+    )
     assert output == "tf plan -var-file foo.vars -var foo=var"
+
 
